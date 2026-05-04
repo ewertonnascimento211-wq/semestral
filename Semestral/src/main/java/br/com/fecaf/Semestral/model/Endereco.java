c8629a0e93befe7159c67a852086898595c09057
Ewerton <ewertonnascimento211@gmail.com>
2026-05-04 10:19:53 -0300

    Criação das classes usuarios e endereço

---
+++ Semestral/src/main/java/br/com/fecaf/Semestral/model/Endereco.java
@@ -0,0 +1,11 @@
+package br.com.fecaf.Semestral.model;
+
+public class Endereco {
+    private Long id;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private int numero;
+
+}
---
+++ Semestral/src/main/java/br/com/fecaf/Semestral/model/Usuario.java
@@ -0,0 +1,12 @@
+package br.com.fecaf.Semestral.model;
+
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private String senha;
+}
+
